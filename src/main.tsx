ba4dba1ec3be57a87464c3f4caa7cd5e15e24b28
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-06-24 21:35:17 -0300

    atualizando o readme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
+import React from "react";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  // HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import {} from "react-router-dom";
+
 import MovieList from "./components/MovieList/index";
 import MovieDetails from "./components/pages/movieDetails";
 import { VisibilityProvider } from "./components/visibilityContext";
@@ -8,15 +15,15 @@ import { SearchMovie } from "./components/inputs/search";
 export const AppRoutes = () => {
   return (
     <VisibilityProvider>
-      <BrowserRouter>
-        <Navbar />
-        <SearchMovie />
-        <Routes>
-          <Route path="*" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<MovieList />} />
-          <Route path="/movie/:id" element={<MovieDetails />} />
-        </Routes>
-      </BrowserRouter>
+      {/* <Router> */}
+      <Navbar />
+      <SearchMovie />
+      <Routes>
+        <Route path="*" element={<Navigate to="home" />} />
+        <Route path="/home" element={<MovieList />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+      </Routes>
+      {/* </Router> */}
     </VisibilityProvider>
   );
 };
